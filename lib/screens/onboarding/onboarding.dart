1bed6e22ad5760ac3b4f1cd58e226cb66112abab
Moaz Refaat <moazrefaat800@gmail.com>
2026-02-09 05:57:35 +0300

    initial commit

---
+++ lib/screens/onboarding/onboarding.dart
@@ -0,0 +1,130 @@
+import 'package:vezeeta/screens/onboarding/sign_in.dart';
+import 'package:flutter/material.dart';
+
+class Onboarding extends StatelessWidget {
+  const Onboarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              ShaderMask(
+                shaderCallback: (Rect bounds) {
+                  return const LinearGradient(
+                    begin: Alignment(0, 0.6),
+                    end: Alignment.bottomCenter,
+                    colors: [Colors.black, Colors.transparent],
+                    stops: [0.0, 1.0],
+                  ).createShader(bounds);
+                },
+                blendMode: BlendMode.dstIn,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 170),
+                  child: Image.asset(
+                    'assets/splash_background.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 70),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset('assets/logo.png', width: screenWidth * 0.12),
+                        const SizedBox(width: 10),
+                        Text(
+                          "Docdoc",
+                          style: TextStyle(
+                            fontSize: screenWidth * 1 / 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 50),
+                    child: Stack(
+                      children: [
+                        ShaderMask(
+                          shaderCallback: (Rect bounds) {
+                            return const LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [Colors.black, Colors.transparent],
+                              stops: [0.5, 0.9],
+                            ).createShader(bounds);
+                          },
+                          blendMode: BlendMode.dstIn,
+                          child: Image.asset(
+                            'assets/doctor.png',
+                            width: double.infinity,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 10,
+                          left: 20,
+                          right: 20,
+                          child: Text(
+                            "Best Doctor\nAppointment App",
+                            style: TextStyle(
+                              fontSize: screenWidth * 0.09,
+                              fontWeight: FontWeight.w700,
+                              color: Color.fromRGBO(36, 124, 255, 1),
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: screenWidth *0.05),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text("Manage and schedule all of your medical appointments easily with Docdoc to get a new experience.",style: TextStyle(
+                          color: Color.fromRGBO(117, 117, 117, 1),
+                          fontWeight: FontWeight.w400,
+                          fontSize: screenWidth * 0.03
+                        ),textAlign: TextAlign.center,),
+                      ),
+                    )
+                ]),
+              ),
+            ],
+          ),
+          SizedBox(height: 40),
+          GestureDetector(
+            onTap: () {Navigator.push(context, MaterialPageRoute(builder: (context) => SignIn()));},
+            child: Container(
+              width: screenWidth * 0.8,
+              height: 70,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(36, 124, 255, 1),
+                borderRadius: BorderRadius.circular(16)
+              ),
+              child: Center(
+                child: Text("Get Started", style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20,
+                  color: Colors.white
+                ),),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/onboarding/sign_in.dart
@@ -0,0 +1,248 @@
+import 'dart:ui';
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+import 'signup.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(254, 255, 255, 1),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 100),
+            Text(
+              "Welcome Back",
+              style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 30,
+                  color: Color.fromRGBO(36, 124, 255, 1)),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "We're excited to have you back, can't wait to see what you've been up to since you last logged in.",
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  color: Color.fromRGBO(117, 117, 117, 1)),
+            ),
+            SizedBox(height: 50),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: "Email",
+                filled: true,
+                fillColor: const Color.fromRGBO(253, 253, 255, 1),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: const BorderSide(
+                      color: Color.fromRGBO(36, 124, 255, 1)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: const BorderSide(
+                      color: Color.fromRGBO(36, 124, 255, 1)),
+                ),
+                hintStyle: const TextStyle(
+                    fontSize: 14, color: Color.fromRGBO(194, 194, 194, 1)),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              keyboardType: TextInputType.visiblePassword,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Password",
+                filled: true,
+                fillColor: const Color.fromRGBO(253, 253, 255, 1),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: const BorderSide(
+                      color: Color.fromRGBO(36, 124, 255, 1)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: const BorderSide(
+                      color: Color.fromRGBO(36, 124, 255, 1)),
+                ),
+                hintStyle: const TextStyle(
+                    fontSize: 14, color: Color.fromRGBO(194, 194, 194, 1)),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Checkbox(
+                  value: false,
+                  onChanged: (value) {},
+                  side:
+                  BorderSide(color: Color.fromRGBO(194, 194, 194, 1)),
+                  activeColor: Color.fromRGBO(36, 124, 255, 1),
+                ),
+                Text(
+                  "Remember Me",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 12,
+                      color: Color.fromRGBO(158, 158, 158, 1)),
+                ),
+                Spacer(),
+                Text(
+                  "Forgot Password?",
+                  style: TextStyle(
+                      color: Color.fromRGBO(36, 124, 255, 1),
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SignIn()));
+              },
+              child: Container(
+                width: screenWidth * 1,
+                height: 70,
+                decoration: BoxDecoration(
+                    color: Color.fromRGBO(36, 124, 255, 1),
+                    borderRadius: BorderRadius.circular(16)),
+                child: Center(
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                        color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Expanded(
+                    child: Divider(
+                      color: Color.fromRGBO(158, 158, 158, 1),
+                    )),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(
+                    "Or sign in with",
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: Color.fromRGBO(158, 158, 158, 1)),
+                  ),
+                ),
+                Expanded(
+                    child: Divider(
+                      color: Color.fromRGBO(158, 158, 158, 1),
+                    )),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Container(
+                width: 46,
+                height: 46,
+                decoration: BoxDecoration(
+                    color: Color.fromRGBO(245, 245, 245, 1),
+                    borderRadius: BorderRadius.circular(100)),
+                child: Image.asset("assets/google.png"),
+              ),
+              SizedBox(width: 40),
+              Container(
+                width: 46,
+                height: 46,
+                decoration: BoxDecoration(
+                    color: const Color.fromRGBO(245, 245, 245, 1),
+                    borderRadius: BorderRadius.circular(100)),
+                child: Image.asset("assets/facebook.png"),
+              ),
+              const SizedBox(width: 40),
+              Container(
+                width: 46,
+                height: 46,
+                decoration: BoxDecoration(
+                    color: const Color.fromRGBO(245, 245, 245, 1),
+                    borderRadius: BorderRadius.circular(100)),
+                child: Image.asset("assets/apple.png"),
+              ),
+            ]),
+            SizedBox(height: 50),
+            RichText(
+              textAlign: TextAlign.center,
+              text: const TextSpan(
+                style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    color: Color.fromRGBO(158, 158, 158, 1)),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: "By logging, you agree to our ",
+                  ),
+                  TextSpan(
+                      text: "Terms & Conditions",
+                      style: TextStyle(color: Colors.black)),
+                  TextSpan(
+                    text: " and ",
+                  ),
+                  TextSpan(
+                      text: "PrivacyPolicy",
+                      style: TextStyle(color: Colors.black)),
+                  TextSpan(
+                    text: ".",
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 25),
+            Center(
+              child: RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      color: Color.fromRGBO(158, 158, 158, 1)),
+                  children: <TextSpan>[
+                    const TextSpan(
+                      text: "Don't have an account yet? ",
+                    ),
+                    TextSpan(
+                        text: 'Sign Up',
+                        style: const TextStyle(
+                            color: Color.fromRGBO(36, 124, 255, 1),
+                            fontWeight: FontWeight.w600),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SignUp()),
+                            );
+                          }),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
